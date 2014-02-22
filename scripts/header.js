f0cf08e46a1da0920105d42eed3f7595b0514a05
ick-seshoka <ick.seshoka@gmail.com>
2014-02-22 20:09:29 +0200

    moving the share group trigger up when the header reduces size

--- scripts/header.js
+++ scripts/header.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
    $(function(){
        //select header object
        
+       var shareGroup = $(".share-group");
        var headerElement = $("#page-header");
         $(window).scroll(function(){
            //check if user has scrolled 100px down
@@ -13,9 +14,11 @@ $(document).ready(function(){
             if($(this).scrollTop() > 40){
                 //add header class
                 headerElement.addClass("hide-small-text");
+                shareGroup.addClass("move-up");
             }else{
                 //remove header class
                 headerElement.removeClass("hide-small-text");
+                shareGroup.removeClass("move-up");
             }
         }); 
    });
